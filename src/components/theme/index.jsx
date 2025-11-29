1677ced2f884487cb1b2e78dd0d9f039f6693b8f
Sayem Kalim <101326225+sayemkalim@users.noreply.github.com>
2025-11-29 22:50:53 +0530

    Change default theme to 'light' in ThemeProvider component

--- src/components/theme/index.jsx
+++ src/components/theme/index.jsx
@@ -7,7 +7,7 @@ const ThemeProviderContext = createContext({
 
 export function ThemeProvider({
   children,
-  defaultTheme = "system",
+  defaultTheme = "light",
   storageKey = "vite-ui-theme",
   ...props
 }) {
