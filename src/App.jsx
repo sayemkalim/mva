fd85cfad04e329f60f0c102764983e78de0c967d
Sayem Kalim <101326225+sayemkalim@users.noreply.github.com>
2026-02-13 22:58:54 +0530

    feat: Enable core application structure by uncommenting theme, notification, and routing providers.

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,13 @@ import NotificationContainer from "./components/notification-container";
 
 const App = () => {
   return (
-    // <ThemeProvider>
-    //   <NotificationProvider>
-    //     <Toaster position="top-center" richColors />
-    //     <NotificationContainer />
-    //     <Router />
-    //   </NotificationProvider>
-    // </ThemeProvider>
-    <div className="text-red-500">Hello World</div>
+    <ThemeProvider>
+      <NotificationProvider>
+        <Toaster position="top-center" richColors />
+        <NotificationContainer />
+        <Router />
+      </NotificationProvider>
+    </ThemeProvider>
   );
 };
 
